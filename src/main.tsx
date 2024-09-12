9aba2ba869866ae6f2cfa9bb8246e00763b60a51
Danil <vda@2i.tusur.ru>
2024-09-13 01:03:36 +0700

    refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { App } from './app/app';
+import { RouterProvider } from 'react-router-dom'
+import Providers from './providers/Providers.tsx'
+import { router } from './pages/router.tsx'
 import '@/app/assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
   </React.StrictMode>,
 )
